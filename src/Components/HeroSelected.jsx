6e9c2fc6460a8f8d3a5b6f1dfd29ba15d64729b1
owen gaudio <Owen@Matthews-MacBook-Pro.local>
2021-03-01 16:45:43 -0500

    last one before my life turns to hell :)

--- src/Components/HeroSelected.jsx
+++ src/Components/HeroSelected.jsx
@@ -3,12 +3,12 @@ import { Card } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import team from "./selected.json"
 
-function HeroSelected({ data }) {
+function HeroSelected({ data, removeCharacter, idx}) {
     return (
         <Card className="h-100 shadow-sm bg-white rounded">
             <Card.Img variant="top" src= {data.pose}/>
                 <Button
-                    onClick={() => team = team.filter((data) => { return data.id !== data.id })}
+                    onClick={() => removeCharacter(idx)}
                     className="mt-auto font-weight-bold"
                     variant="danger"
                     block>
--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -1,23 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Row } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 import HeroSelected  from './HeroSelected';
 import "./HeroStyling.scss";
-import team from "./selected.json";
+import initialTeam from "./selected.json";
+import image from './teambackground.jpg';
+
+var sectionStyle = {
+   backgroundImage: `url(${image})`
+}
 
 function Team() {
+
+  const [team,setTeam] = useState([])
+  useEffect(() => {
+    setTeam(initialTeam)
+  },[])
+
+  function removeCharacter(idx){
+    const newTeam = [...team]
+    newTeam.splice(idx,1)
+    setTeam(newTeam)
+  }
+
   return (
+    <div style={sectionStyle}>
     <Container>
        <Row>
-         {team.map(data => (
+         {team.map((data, idx) => (
            <Col xs={3} sm={3} md={4} lg={4} xl={4} className="mb-5" key={`${data.id}`}>
-             <HeroSelected data={data}/>
+             <HeroSelected data={data} removeCharacter={removeCharacter} idx={idx}/>
           </Col>
         ))}
       </Row>
      </Container>
+     </div>
  );
 }
 
